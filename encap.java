596c3ad335efcb1c69a44ffad218c8b2c4c3162e
VismayRaul <vismayraul@gmail.com>
2020-11-15 22:53:10 +0530

    Adding: Sample Program on Encapsulation of Mobile Details

---
+++ encap.java
@@ -0,0 +1,11 @@
+
+public class encap {
+		public static void main (String args[] ) {
+			mobile obj = new mobile();
+			obj.setIp (1256425);
+			obj.setName ("Oppo");
+			obj.setColour ("Red");
+			System.out.println("Company name-" + obj.getName() +  " Mobile colour-" + obj.getColour()+ " IP Address-" +obj.getIp());
+		}
+}
+
---
+++ mobile.java
@@ -0,0 +1,29 @@
+public class mobile {
+	private String name;
+	private String colour;
+	private int ip;
+	
+	public int getIp() {
+		return ip;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getColour() {
+		return colour;
+	}
+	
+	public void setIp( int newIp) {
+		ip = newIp;
+	}
+	
+	public void setName(String newName) {
+		name = newName;
+	}
+	
+	public void setColour(String newColour) {
+		colour = newColour;
+	}
+}
